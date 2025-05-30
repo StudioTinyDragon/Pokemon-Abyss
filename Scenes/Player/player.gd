dbdf0a3b612cca168bff3b75d1c3bfccfcef45a1
PlsSendHelpNow <erik.balze@gmail.com>
2025-05-30 22:31:56 +0200

    start des Projects

---
+++ Scenes/Player/player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+const SPEED = 220.0
+
+func _process(delta: float) -> void:
+	var input_vector := Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+
+	if input_vector.length() > 0:
+		input_vector = input_vector.normalized()
+		position += input_vector * SPEED * delta
---
+++ Scenes/Units/test_pokemon.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+@export var Name : String
+@export var PokedexNR : int
+@export var TYP: Array[String]
+
+@export var Gender : String
+@export var Level: int
+@export var Exp: int
+@export var Moves: Array[String]
+@export var Ability : String
+
+@export var currentHP: int
+@export var Move1AP: int
+@export var Move2AP: int
+@export var Move3AP: int
+@export var Move4AP: int
+@export var StatusEffect: Array[String]
+
+@export var maxHP: int
+@export var Attack: int
+@export var SPAttack: int
+@export var Defense: int
+@export var SPDefense: int
+@export var Iniative: int
+
+@export var CatchRate: int
